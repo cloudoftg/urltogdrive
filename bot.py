8f9e5891730c82adce715333b5752f0f4228908e
cloudoftg <68091333+cloudoftg@users.noreply.github.com>
2020-07-11 15:46:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -280,6 +280,7 @@ def UPLOAD(update, context):
             if DownloadStatus:
                 sent_message.edit_text(TEXT.UPLOADING)
             try:
+                
                 await message.edit(
                     text="{}\n {}".format(
                         ud_type,
